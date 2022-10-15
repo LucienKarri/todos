92c7f09636ba31da0c8f804565b62a7a0415147a
lkarri <markovkacorp@gmail.com>
2022-10-15 12:36:36 +0500

    delete test func

--- src/index.js
+++ src/index.js
@@ -5,6 +5,5 @@ import TodoApp from './components/todoApp';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-const test = () => {};
-test();
+
 root.render(<TodoApp />);
